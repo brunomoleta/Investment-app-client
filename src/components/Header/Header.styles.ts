3f903fcf6bfe40a9be12ec6ee19e5b47c575fd83
Bruno Moleta <brunomoleta@pm.me>
2024-02-06 19:11:40 -0300

    The previously existing AccessType component was removed as part of this update, while some styling changes have also been made to the Header component.

--- src/components/Header/Header.styles.ts
+++ src/components/Header/Header.styles.ts
@@ -2,9 +2,8 @@
 import styled from "styled-components";
 
 export const StyledHeader = styled.header`
-  position: sticky;
-  top: 0;
-  z-index: 1;
+  position: relative;
+    z-index: 1;
 
   display: flex;
   flex-wrap: wrap;
---
+++ src/schemas/loginSchema/index.ts
@@ -0,0 +1,2 @@
+export * from './loginSchema';
+export { default } from './loginSchema';
