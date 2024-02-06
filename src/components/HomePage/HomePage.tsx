49af21cf706203d6480e6cf8752689a67d334cb2
Bruno Moleta <brunomoleta@pm.me>
2024-02-06 11:40:15 -0300

    Add BlogFeed component to HomePage
    Added the BlogFeed component to the HomePage.tsx file. This component will display a list of blog posts on the home page.

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import Template from '@/components/Template';
-import Hero from '@/components/HomePage/Hero';
-import WhyUs from '@/components/HomePage/WhyUs';
+import React from "react";
+import Template from "@/components/Template";
+import Hero from "@/components/HomePage/Hero";
+import WhyUs from "@/components/HomePage/WhyUs";
+import BlogFeed from "@/components/BlogFeed";
 
 function HomePage() {
   return (
     <Template>
       <Hero />
       <WhyUs />
-
+      <BlogFeed />
     </Template>
   );
 }
