685ff4d43e39e9f9dfeb4a0f91736b3107900a5d
Bruno Moleta <brunomoleta@pm.me>
2024-02-06 08:57:35 -0300

    Create Header component with styles and items
    This commit introduces multiple new files to set up the Header component, including individual items and their styles.

---
+++ src/components/Template/Template.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "@/components/Header";
+import { ChildrenProps } from "@/types/children";
+import Footer from '@/components/Footer';
+
+const Template: React.FC<ChildrenProps> = ({ children }) => {
+  return (
+    <>
+      <Header />
+      {children}
+      <Footer/>
+    </>
+  );
+};
+
+export default Template;
