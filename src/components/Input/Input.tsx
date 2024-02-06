0e327237bb742b11ded68395e8452a36dda686fb
Bruno Moleta <brunomoleta@pm.me>
2024-02-06 18:16:56 -0300

    Made the "type" attribute in IInput interface optional. Implemented password visibility functionality by adopting the useUserContext() method in the Input component.

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,40 +1,48 @@
-'use client';
-import React from 'react';
-import {ErrorSpan, Field, Label, PasswordButton, StyledInput} from "@/components/Input/Input.styles";
-import {Eye, EyeOff} from "react-feather";
-import {IInput} from "@/types/input";
+"use client";
+import React from "react";
+import {
+  ErrorSpan,
+  Field,
+  Label,
+  PasswordButton,
+  StyledInput,
+} from "@/components/Input/Input.styles";
+import { Eye, EyeOff } from "react-feather";
+import { IInput } from "@/types/input";
+import {useUserContext} from "@/providers/UserProvider";
+import {IUserContext} from "@/types/user";
 
 const Input = React.forwardRef(
-    (
-        { error, id, type = "text", label, ...delegated }: IInput,
-        ref: React.ForwardedRef<HTMLInputElement>,
-    ) => {
-      // const { changePasswordVisibility } = useUserContext() as IUserContext;
-      return (
-          <>
-            <Field>
+  (
+    { error, id, type = "text", label, ...delegated }: IInput,
+    ref: React.ForwardedRef<HTMLInputElement>
+  ) => {
+    const { changePasswordVisibility } = useUserContext() as IUserContext;
+    return (
+      <>
+        <Field>
+          <Label htmlFor={id}>
+            {label.charAt(0).toUpperCase() + label.slice(1)}
+          </Label>
+          <ErrorSpan>{error && error.message}</ErrorSpan>
 
-                <Label htmlFor={id}>
-                  {label.charAt(0).toUpperCase() + label.slice(1)}
-                </Label>
-                <ErrorSpan>{error && error.message}</ErrorSpan>
-
-              <div style={{ position: "relative" }}>
-                <StyledInput type={type} id={id} ref={ref} {...delegated} />
-                {label.toLowerCase().includes("senha") && (
-                    <PasswordButton
-                        onClick={(event) => {
-                          event.preventDefault();
-                        }}
-                    >
-                      {type === "text" ? <Eye /> : <EyeOff />}
-                    </PasswordButton>
-                )}
-              </div>
-            </Field>
-          </>
-      );
-    },
+          <div style={{ position: "relative" }}>
+            <StyledInput type={type} id={id} ref={ref} {...delegated} />
+            {label.toLowerCase().includes("senha") && (
+              <PasswordButton
+                onClick={(event) => {
+                  event.preventDefault();
+                  changePasswordVisibility();
+                }}
+              >
+                {type === "text" ? <Eye /> : <EyeOff />}
+              </PasswordButton>
+            )}
+          </div>
+        </Field>
+      </>
+    );
+  }
 );
-Input.displayName = "Input"
+Input.displayName = "Input";
 export default Input;
