2c631442225c8c63d3700c9a7e47a8c9501bb088
Bruno Moleta <brunomoleta@pm.me>
2024-02-08 19:56:56 -0300

    Update LoginForm styles for better layout
    Changes include modifications to components such as the form positioning, width, box shadow, and the form's button's margins and border radius.

--- src/components/Login/LoginForm/LoginForm.style.ts
+++ src/components/Login/LoginForm/LoginForm.style.ts
@@ -2,10 +2,25 @@
 import styled from "styled-components";
 
 export const Form = styled.form`
+    position: relative;
+    width: 100%;
+    max-width: 400px;
     display: flex;
     flex-flow: column;
-    gap: var(--s-1);
-    > button {
-        width: 100%;
+    border-radius: var(--s0);
+    box-shadow: 3px 3px 10px 3px var(--grey-400-t);
+
+    > fieldset:first-child {
+        margin-block-start: var(--s-1);
     }
+    
+    > button {
+        margin-block-start: var(--s0);
+        width: 100%;
+        max-width: none;
+        
+        border-radius: 0 0 10px 10px;
+    }
+
+    &:focus-within {}
 `
