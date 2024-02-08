1c9c8b39612c567c34c7be23b50d37c96d34431e
Bruno Moleta <brunomoleta@pm.me>
2024-02-08 15:19:48 -0300

    Add investor signup page and update login page
    A new signup page has been created for investors, using existing components for its layout and functionality. Additionally, a link to this new signup page has been added onto the investor login page as an alternative sign-in option.

--- src/app/investor/page.tsx
+++ src/app/investor/page.tsx
@@ -1,12 +1,12 @@
 import LoginForm from "../../components/Login/LoginForm";
-import LoginPage from "@/components/LoginPage";
+import LoginPage from "../../components/Login/LoginPage";
 import SignInAlternative from "@/components/SignInAlternative";
 
 export default function InvestorLogin() {
   return (
     <LoginPage
       loginForm={<LoginForm userType="investor" />}
-      signInAlternative={<SignInAlternative />}
+      signInAlternative={<SignInAlternative href={'/investor/signup'} />}
     />
   );
 }
