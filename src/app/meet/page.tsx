ef0bab6b2beeadc2c1c0f3968d0c9dca36d1aed9
Bruno Moleta <brunomoleta@pm.me>
2024-02-09 18:28:58 -0300

    Add new MeetAdvisors page component
    Introduced a new MeetAdvisors component in the application. This component fetches advisor data asynchronously from the server, displays a spinner during loading, and ultimately renders a list of advisors. It leverages the useAdvisorContext and useUtilsContext hooks for fetching data and managing the loading state respectively.

---
+++ src/app/meet/page.tsx
@@ -0,0 +1,36 @@
+"use client";
+import React from "react";
+import Template from "@/components/Template";
+import { IAdvisorContext } from "@/types/advisorContext";
+import { useAdvisorContext } from "@/providers/AdvisorProvider";
+import RenderManyAdvisors from "@/components/RenderManyAdvisors";
+import { useUtilsContext } from "@/providers/UtilsProvider";
+import { IUtilsContext } from "@/types/utils";
+import Spinner from "@/components/Spinner";
+import { MainWrapper } from "@/components/RenderManyAdvisors/ManyAdvisors.style";
+import {Heading} from "@/components/HomePage/WhyUs/WhyUsText/WhyUsText.style";
+
+function MeetAdvisors() {
+  const { getAdvisorsNoAuth } = useAdvisorContext() as IAdvisorContext;
+  const { isLoading, setIsLoading } = useUtilsContext() as IUtilsContext;
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      await getAdvisorsNoAuth();
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, []);
+  return (
+    <Template>
+      <MainWrapper>
+        <Heading>Nossos Especialistas</Heading>
+        {isLoading ? <Spinner /> : <RenderManyAdvisors />}
+      </MainWrapper>
+    </Template>
+  );
+}
+
+export default MeetAdvisors;
