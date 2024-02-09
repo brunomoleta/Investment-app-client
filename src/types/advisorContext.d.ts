afb6068d606a779dcc07dbebd42e5482efd1c7bd
Bruno Moleta <brunomoleta@pm.me>
2024-02-09 13:27:02 -0300

    Add new advisorContext type definitions
    A new file, advisorContext.d.ts, has been added to the types directory.

---
+++ src/types/advisorContext.d.ts
@@ -0,0 +1,11 @@
+import {IAdvisor} from "@/types/users";
+import React from "react";
+
+export interface IAdvisorContext {
+    getAdvisorsNoAuth: () => Promise<void>;
+
+    advisors: IAdvisor[] | null;
+    setAdvisors: React.Dispatch<React.SetStateAction<IAdvisor[] | null>>;
+
+    handleMeetClick: (event: React.MouseEvent<HTMLButtonElement>)=> Promise<void>;
+}
