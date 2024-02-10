78114e199557d6a43ea748edc9cc6585752540af
Bruno Moleta <brunomoleta@pm.me>
2024-02-10 14:59:41 -0300

    Add ChooseAdvisor component and modify ChooseUserForm
    Implemented the ChooseAdvisor component to facilitate advisor selection during user sign-in. Updated the ChooseUserForm component, incorporating user context and setting default focus to user selection input.

--- src/components/ChooseUserForm/ChooseUserForm.tsx
+++ src/components/ChooseUserForm/ChooseUserForm.tsx
@@ -4,6 +4,8 @@ import Button from "@/components/Button";
 import { Form } from "@/components/Login/LoginForm/LoginForm.style";
 import { useForm } from "react-hook-form";
 import { useRouter } from "next/navigation";
+import { useUserContext } from "@/providers/UserProvider";
+import { IUserContext } from "@/types/userContext";
 
 function ChooseUserForm() {
   const router = useRouter();
@@ -11,27 +13,34 @@ function ChooseUserForm() {
   const investorId = `${id}-investor`;
   const advisorId = `${id}-advisor`;
 
-  const { register, handleSubmit } = useForm<RadioOutput>();
+  const { setUserType } = useUserContext() as IUserContext;
+
+  const { register, setFocus, handleSubmit } = useForm<RadioOutput>({
+    defaultValues: { radioOutput: "investor" },
+  });
+
+  React.useEffect(()=>{setFocus("radioOutput")},[])
 
   function onSubmit(formData: RadioOutput): void {
     router.push(`/${formData.radioOutput}`);
+    setUserType(formData.radioOutput);
   }
 
   return (
     <Form onSubmit={handleSubmit(onSubmit)}>
       <Input
         {...register("radioOutput")}
-        id={advisorId}
-        value="advisor"
-        type="radio"
-        label="assessor"
-      />
-      <Input
-        {...register("radioOutput")}
         id={investorId}
         value="investor"
         type="radio"
         label="investidor"
+      />
+      <Input
+        {...register("radioOutput")}
+        id={advisorId}
+        value="advisor"
+        type="radio"
+        label="assessor"
       />
       <Button type="submit" content="Avançar" />
     </Form>
