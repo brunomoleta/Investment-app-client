2202a62f982634f57caf0f5eab1710914e333bbc
Bruno Moleta <brunomoleta@pm.me>
2024-02-10 14:53:07 -0300

    Implement loading state in Button component
    The Button component is now using isLoading from the UtilsContext to display a Spinner while content is loading. The button will also become disabled during the loading state.

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,18 +1,28 @@
 "use client";
 import React from "react";
 import { ButtonProps } from "@/types/children";
-import {ButtonStyle, PrimaryButton} from "@/styled-components/Button.style";
+import { ButtonStyle, PrimaryButton } from "@/styled-components/Button.style";
 import Link from "next/link";
+import { useUtilsContext } from "@/providers/UtilsProvider";
+import { IUtilsContext } from "@/types/utils";
+import Spinner from "@/components/Spinner";
 
-const Button: React.FC<ButtonProps> = ({ isPrimary = true, href, content, ...delegated}) => {
-    const Component = isPrimary ? PrimaryButton : ButtonStyle;
-    const props = {
-        as: href ? Link : "button",
-        href: href,
-        children: content,
-        ...delegated,
-    };
+const Button: React.FC<ButtonProps> = ({
+  isPrimary = true,
+  href,
+  content,
+  ...delegated
+}) => {
+  const { isLoading } = useUtilsContext() as IUtilsContext;
 
-    return <Component {...props} />;
+  const Component = isPrimary ? PrimaryButton : ButtonStyle;
+  const props = {
+    as: href ? Link : "button",
+    href: href,
+    children: isLoading ? <Spinner /> : content,
+    ...delegated,
+  };
+
+  return <Component disabled={isLoading} {...props} />;
 };
 export default Button;
