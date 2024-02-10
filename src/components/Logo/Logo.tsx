0a75b5c0118fafa40ac53738cc8b19c524b29d10
Bruno Moleta <brunomoleta@pm.me>
2024-02-10 14:58:39 -0300

    Refactor Template component and enhance Logo click action
    User-specific context and actions were transfered from the Template component to the Logo component onClick event.

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,14 +1,19 @@
+'use client'
 import React from "react";
 import { LogoStyle } from "@/components/Logo/Logo.style";
 import Image from "next/image";
+import { useUtilsContext } from "@/providers/UtilsProvider";
+import { IUtilsContext } from "@/types/utils";
 
 type LogoProps = {
   src: string;
 };
 
 const Logo: React.FC<LogoProps> = ({ src }) => {
+  const { logoClick } = useUtilsContext() as IUtilsContext;
+
   return (
-    <LogoStyle href={"/"}>
+    <LogoStyle onClick={logoClick}>
       <Image alt="app logo" src={src} />
     </LogoStyle>
   );
--- src/components/Template/Template.tsx
+++ src/components/Template/Template.tsx
@@ -3,25 +3,8 @@ import React from "react";
 import Header from "@/components/Header";
 import { ChildrenProps } from "@/types/children";
 import Footer from "@/components/Footer";
-import { useUserContext } from "@/providers/UserProvider";
-import { IUserContext } from "@/types/user";
 
 const Template: React.FC<ChildrenProps> = ({ children }) => {
-  const {
-    isLoggedIn,
-    retrieveUserFromId,
-  } = useUserContext() as IUserContext;
-
-  React.useEffect(() => {
-
-
-    const asyncFunc = async () => {
-        await retrieveUserFromId();
-
-    };
-
-    asyncFunc();
-  }, [isLoggedIn]);
   return (
     <>
       <Header />
