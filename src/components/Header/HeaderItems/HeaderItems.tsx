864bc5d6ca8b4661b65853f1683d7c52ad6c37bf
Bruno Moleta <brunomoleta@pm.me>
2024-02-12 13:33:25 -0300

    RefactorModify HeaderItem link and wrap HeaderItems in nav
    Wrapped the HeaderItems component within a <nav> tag for improved semantic structure.

--- src/components/Header/HeaderItems/HeaderItems.tsx
+++ src/components/Header/HeaderItems/HeaderItems.tsx
@@ -1,16 +1,19 @@
-'use client'
-import React from 'react';
-import { headeritems } from '@/services/data';
-import { Items } from '@/components/Header/HeaderItems/HeaderItems.style';
-import HeaderItem from '@/components/Header/HeaderItems/HeaderItem';
+"use client";
+import React from "react";
+import { headeritems } from "@/services/data";
+import { Items } from "@/components/Header/HeaderItems/HeaderItems.style";
+import HeaderItem from "@/components/Header/HeaderItems/HeaderItem";
 
 function HeaderItems() {
-  return <Items>
-    {headeritems.map((item, index) =>
-      <HeaderItem item={item} key={index}/>
-    )}
-
-  </Items>;
+  return (
+    <nav>
+      <Items>
+        {headeritems.map((item, index) => (
+          <HeaderItem item={item} key={index} />
+        ))}
+      </Items>
+    </nav>
+  );
 }
 
 export default HeaderItems;
