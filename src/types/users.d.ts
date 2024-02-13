1292c654ae9b2c7319dd6b806b28b293af86ec5c
Bruno Moleta <brunomoleta@pm.me>
2024-02-13 12:28:54 -0300

    Add new interfaces to user types
    Added two new interfaces, IFullAdvisor and IFullInvestor, to provide additional details for rendering advisors and investors.

--- src/types/users.d.ts
+++ src/types/users.d.ts
@@ -21,6 +21,14 @@ export interface IAdvisor extends IUser {
   speciality_id: string;
 }
 
+export interface IFullAdvisor extends IAdvisor {
+  investors: IInvestor[];
+}
+
+export interface IFullInvestor extends IInvestor {
+  advisor: IAdvisor;
+}
+
 export interface IInvestor extends IUser {
   amount: InvestmentAmount;
   advisor_id: string;
