f8eaf0a67d38c06ca00d7355d32edf479e8f5240
Bruno Moleta <brunomoleta@pm.me>
2024-02-14 18:24:30 -0300

    Update Footer and Logo components for dashboard view
    Added a condition to only show FooterAnchors and AccessButton in Footer when not in dashboard view. Modified Logo component to include routing, user context and a tooltip according to view context.

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,4 +1,3 @@
-"use client";
 import { StyledFooter } from "@/components/Footer/Footer.style";
 import LogoDark from "../../assets/images/logo-dark.svg";
 import Logo from "@/components/Logo";
@@ -6,14 +5,17 @@ import FooterAnchors from "@/components/Footer/FooterAnchors";
 import SocialMediaIcons from "@/components/Footer/SocialMediaIcons";
 import AccessButton from "@/components/Button/AccessButton";
 
-function Footer() {
+function Footer({ isDashboard = false }: { isDashboard?: boolean }) {
   return (
     <StyledFooter>
       <Logo src={LogoDark} />
       <SocialMediaIcons />
-
-      <FooterAnchors />
-      <AccessButton />
+      {!isDashboard && (
+        <>
+          <FooterAnchors />
+          <AccessButton />
+        </>
+      )}
       <p>© Easybank 2024. Todos os direitos reservados.</p>
     </StyledFooter>
   );
--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -4,6 +4,9 @@ import { LogoStyle, StyledImage } from "@/components/Logo/Logo.style";
 import Image, { StaticImageData } from "next/image";
 import { useUtilsContext } from "@/providers/UtilsProvider";
 import { IUtilsContext } from "@/types/utils";
+import { useRouter } from "next/navigation";
+import { useUserContext } from "@/providers/UserProvider";
+import { IUserContext } from "@/types/userContext";
 
 interface LogoProps {
   src: string | StaticImageData;
@@ -12,9 +15,19 @@ interface LogoProps {
 
 const Logo: React.FC<LogoProps> = ({ src, isDashboard = false }) => {
   const { logoClick } = useUtilsContext() as IUtilsContext;
+  const { userType } = useUserContext() as IUserContext;
+  const router = useRouter();
+
+  const tooltip: string = isDashboard
+    ? "Voltar para dashboard"
+    : "Ir para homepage";
+
+  const handleClick = isDashboard
+    ? () => router.push(`/${userType}/dashboard`)
+    : logoClick;
 
   return (
-    <LogoStyle onClick={logoClick} aria-label="retornar a página inicial">
+    <LogoStyle title={tooltip} onClick={handleClick} aria-label={tooltip}>
       {!isDashboard ? (
         <Image alt="app logo" src={src} />
       ) : (
