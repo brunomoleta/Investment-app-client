afbbc287f507f78e69ff4afda95e923be7a21fc2
Bruno Moleta <brunomoleta@pm.me>
2024-02-16 09:34:23 -0300

    Create PersonalInfo component and refactor usage
    Code from 'the  AdvisorPersonalInfo function was refactored and moved to the PersonalInfo.

--- src/app/advisor/dashboard/personal-info/page.tsx
+++ src/app/advisor/dashboard/personal-info/page.tsx
@@ -1,33 +1,12 @@
-"use client";
 import { Heading } from "@/components/Login/LoginPage/LoginPage.style";
 import React from "react";
-import { useUserContext } from "@/providers/UserProvider";
-import { IUserContext } from "@/types/userContext";
-import Spinner from "@/components/Spinner";
-import { useUtilsContext } from "@/providers/UtilsProvider";
-import { IUtilsContext } from "@/types/utils";
-import UpdatePersonalInfo from "@/components/Dashboard/Main/PersonalInfo/UpdatePersonalInfo";
-import RenderPersonalInfo from "@/components/Dashboard/Main/PersonalInfo/RenderPersonalInfo";
+import PersonalInfo from "@/components/Dashboard/Main/PersonalInfo";
 
 export default function AdvisorPersonalInfo() {
-  const { activeUser } = useUserContext() as IUserContext;
-  const { isEditing } = useUtilsContext() as IUtilsContext;
-
-  function isRendered() {
-    if (!activeUser) {
-      return <Spinner />;
-    }
-    if (isEditing) {
-      return <UpdatePersonalInfo />;
-    } else {
-      return <RenderPersonalInfo />;
-    }
-  }
-
   return (
     <>
       <Heading>Informações pessoais</Heading>
-      {isRendered()}
+      <PersonalInfo />
     </>
   );
 }
