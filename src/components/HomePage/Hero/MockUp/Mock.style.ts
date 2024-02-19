c6f393d82444e435ded11e7446e1274775924448
Bruno Moleta <brunomoleta@pm.me>
2024-02-19 19:39:31 -0300

    Refactor mockup component for responsive design
    The existing mockup component has been refactored to support both mobile and desktop viewports. Two separate image components, BackgroundMobile and BackgroundDesktop, have replaced the previous MockBackground for proper rendering on different screen sizes. Styling for DevicesImage, formerly MockImage, has been modified for positioning adjustments on larger screens.

--- src/components/HomePage/Hero/MockUp/Mock.style.ts
+++ src/components/HomePage/Hero/MockUp/Mock.style.ts
@@ -1,23 +1,55 @@
 'use client'
+
 import Image from 'next/image';
 import styled from 'styled-components';
-export const MockImage = styled(Image)`
+
+export const DevicesImage = styled(Image)`
     width: 90%;
     height: auto;
     margin-block-start: -40%;
     opacity: .99;
+
+
+    @media (min-width: 920px) {
+        width: calc(var(--mockup-image-width) * .85px);
+        height: calc(var(--mockup-image-height) * .85px);
+        position: relative;
+        right: 220px;
+        top: 180px;
+    }
 `
 
-export const MockBackground = styled(Image)`
+export const BackgroundMobile = styled(Image)`
     position: absolute;
     top: -36%;
     right: 0;
     width: 100svw;
     height: auto;
+    @media (min-width: 920px) {
+        display: none;
+    }
 `
+export const BackgroundDesktop = styled(Image)`
+    position: relative;
+    top: -180px;
+    right: -570px;
+    max-width: 800px;
+    height: auto;
+    scale: .98;
+    @media (max-width: 919px) {
+        display: none;
+    }
+`
+
 export const Wrapper = styled.div`
     position: relative;
     width: 100%;
     display: flex;
     justify-content: center;
+    @media (min-width: 920px) {
+        width: 800px;
+        position: absolute;
+        margin: auto;
+        right: 0;
+    }
 `
--- src/components/HomePage/Hero/MockUp/MockUp.tsx
+++ src/components/HomePage/Hero/MockUp/MockUp.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
-import Mock from '@/assets/images/image-mockups.png';
-import MockBackgroundImage from '../../../../assets/images/bg-intro-mobile.svg';
-import { MockBackground, MockImage, Wrapper } from '@/components/HomePage/Hero/MockUp/Mock.style';
-
+import React from "react";
+import Mock from "@/assets/images/image-mockups.png";
+import Desktop from "../../../../assets/images/bg-intro-desktop.svg";
+import Mobile from "../../../../assets/images/bg-intro-mobile.svg";
+import {
+  BackgroundDesktop,
+  BackgroundMobile,
+  DevicesImage,
+  Wrapper,
+} from "@/components/HomePage/Hero/MockUp/Mock.style";
 
 function MockUp() {
   return (
     <Wrapper>
-      <MockBackground src={MockBackgroundImage} alt="" />
-      <MockImage src={Mock} alt="app of the product running" />
+      <BackgroundMobile src={Mobile} alt="" />
+      <BackgroundDesktop src={Desktop} alt="" />
+      <DevicesImage src={Mock} alt="app of the product running" />
     </Wrapper>
   );
 }
