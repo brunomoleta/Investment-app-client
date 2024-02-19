f2215c7d9825266d07eef00e502f6a400221f739
Bruno Moleta <brunomoleta@pm.me>
2024-02-19 19:39:52 -0300

    Introduce WidthWrapper to Hero component
    A WidthWrapper has been added to the Hero component in order to handle varying screen sizes more effectively. Alongside this change, padding and position adjustments have been made in the Wrapper styled component for better visual compatibility across different viewports.

--- src/components/HomePage/Hero/Hero.style.ts
+++ src/components/HomePage/Hero/Hero.style.ts
@@ -5,6 +5,10 @@ import {Section} from "@/styled-components/MaxWidth.style";
 export const Wrapper = styled(Section)`
     background-color: var(--grey-100);
     padding-inline: 0;
-    padding-block: 0 var(--s2);
-
+    padding-block: 0 var(--s3);
+    margin-inline: auto;
+    
+    @media (min-width: 799px) {
+        position: relative;
+    }
 `
