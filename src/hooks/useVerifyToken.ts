b480eb821af202ce07c43609dc0e8c81c0d39b80
Bruno Moleta <brunomoleta@pm.me>
2024-02-19 13:08:47 -0300

    Implement token verification for dashboard access
    A new hook named 'useVerifyToken' has been created and implemented across different dashboards to verify user authentication. If the token is absent, user is redirected to the login page.

---
+++ src/hooks/useVerifyToken.ts
@@ -0,0 +1,13 @@
+import { getCookie } from "cookies-next";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+export function useVerifyToken(): void {
+  const token = getCookie("token", { cookies });
+  if (!token) {
+    console.log(
+      "Tu não tem acesso a esta página sem autenticação. Por favor faça login."
+    );
+    redirect("/");
+  }
+}
