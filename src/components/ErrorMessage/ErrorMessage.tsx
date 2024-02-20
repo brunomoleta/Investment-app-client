ccec9b59ea822e6b96958c43a9e30e0cc6aea66f
Bruno Moleta <brunomoleta@pm.me>
2024-02-20 12:00:16 -0300

    Add ErrorMessage component
    This commit introduces a new ErrorMessage component which is a reusable UI element for displaying error messages. The component can be customized with dynamic child content.

---
+++ src/components/ErrorMessage/ErrorMessage.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Heading } from "@/components/HomePage/WhyUs/WhyUsText/WhyUsText.style";
+import { ChildrenProps } from "@/types/children";
+import {Wrapper} from "@/components/BlogFeed/BlogFeed.style";
+
+function ErrorMessage({ children }: ChildrenProps) {
+  return (
+    <Wrapper>
+      <Heading>Erro de conexão com a rede</Heading>
+      <p>{children}</p>
+    </Wrapper>
+  );
+}
+
+export default ErrorMessage;
